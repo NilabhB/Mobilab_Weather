35cd42fcf2d0958613062755128b5561ffc4bdb2
Nilabh Nayan Borthakur <borthakur.nilabh@gmail.com>
2023-03-31 19:13:47 +0530

    Initial Commit

---
+++ app/src/main/java/com/mobilab/weather/WeatherRVAdapter.java
@@ -0,0 +1,82 @@
+package com.mobilab.weather;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<WeatherRVModel> weatherRVModelArrayList;
+
+    public WeatherRVAdapter(Context context, ArrayList<WeatherRVModel> weatherRVModelArrayList) {
+        this.context = context;
+        this.weatherRVModelArrayList = weatherRVModelArrayList;
+    }
+
+//    public WeatherRVAdapter(Context context) {
+//        this.context = context;
+//        weatherRVModelArrayList = new ArrayList<>();
+//    }
+//
+//    public void setWeatherRVModelArrayList(ArrayList<WeatherRVModel> list) {
+//        weatherRVModelArrayList = list;
+//        notifyDataSetChanged();
+//    }
+
+    @NonNull
+    @Override
+    public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_rv_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherRVAdapter.ViewHolder holder, int position) {
+
+        WeatherRVModel model = weatherRVModelArrayList.get(position);
+        holder.temperatureTV.setText(model.getTemperature()+"°C");
+        Picasso.get().load("http:".concat(model.getIcon())).into(holder.conditionIV);
+        holder.windTV.setText(model.getWindSpeed()+"Km/h");
+        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        SimpleDateFormat output = new SimpleDateFormat("hh:mm aa");
+        try {
+            Date t = input.parse(model.getTime());
+            holder.timeTV.setText(output.format(t));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherRVModelArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView windTV, temperatureTV, timeTV;
+        private ImageView conditionIV;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            windTV = itemView.findViewById(R.id.idTVWindSpeed);
+            temperatureTV = itemView.findViewById(R.id.idTVTemperature);
+            timeTV = itemView.findViewById(R.id.idTVTime);
+            conditionIV = itemView.findViewById(R.id.idTVCondition);
+        }
+    }
+}
